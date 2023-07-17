6cebafbeea121c90de97eea1255fbbc278e1ae87
Dev-nenda <jooeon1104@gmail.com>
2023-07-17 11:20:19 +0900

    login_css

--- MY_PROJECT/settings.py
+++ MY_PROJECT/settings.py
@@ -174,8 +174,9 @@ STATIC_URL = 'static/'
 
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
+    os.path.join(BASE_DIR, "static", 'accounts'),
 ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
